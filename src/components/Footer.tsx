0badc4a85139b8935a80c5ccd70fd7836ed08584
Simon Freiermuth <simi.freiermuth@bluewin.ch>
2025-05-31 17:37:59 +0200

    state all used libs and licenses

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,24 @@
+import { useRef } from "react";
+import { AboutModal } from "./About";
+
 export function Footer() {
+    const modalRef = useRef<HTMLDialogElement>(null);
+    const openModal = () => {
+        modalRef.current?.showModal();
+    };
+
     return (
         <footer className="footer footer-horizontal footer-center text-base-content p-4">
             <nav className="grid grid-flow-col gap-4">
-                <a className="link link-hover">TV Zeiningen</a>
-                <a className="link link-hover">About</a>
-                <a className="link link-hover">GitHub</a>
+                <a href="https://tvzeiningen.ch" target="_blank" className="link link-hover">
+                    TV Zeiningen
+                </a>
+                <a onClick={openModal} className="link link-hover">About</a>
+                <a href="https://github.com/tvzeiningen" target="_blank" className="link link-hover">
+                    GitHub
+                </a>
+
+                <AboutModal ref={modalRef} />
             </nav>
         </footer>
     );
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,40 @@
-import { defineConfig } from 'vite'
+import { defineConfig, type Plugin, type ResolvedConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { getProjectLicenses, type ILicense } from "generate-license-file";
 
-// https://vite.dev/config/
+function licenses(): Plugin {
+  const virtualModuleId = 'virtual:licenses'
+  const resolvedVirtualModuleId = '\0' + virtualModuleId
+  const licenses: ILicense[] = []
+
+  return {
+    name: 'licenses-plugin',
+    resolveId(id: string) {
+      if (id === virtualModuleId) {
+        return resolvedVirtualModuleId
+      }
+    },
+    async configResolved(config: ResolvedConfig) {
+      const packageJson = '/package.json'
+      config.logger.info(packageJson)
+      const l = await getProjectLicenses(config.envDir + '/package.json', {
+        exclude: ["@esbuild"], // avoid getProjectLicenses getting stuck in missing directory
+      })
+      licenses.push(...l)
+    },
+    load(id: string) {
+      if (id === resolvedVirtualModuleId) {
+        return `export default ${JSON.stringify(licenses)}`
+      }
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    licenses(),
+  ],
 })
